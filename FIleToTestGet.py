3fea868ae5424fab55b9af0c0e09d26490174177
russbramblett <russbramblett@gmail.com>
2018-06-26 10:10:55 -0600

    none

--- FIleToTestGet.py
+++ FIleToTestGet.py
@@ -3,6 +3,6 @@
 numbers = [i for i in range(100)]
 
 for i in range(len(numbers)):
-    numbers[i] = numbers[i]**2
+    numbers[i] = numbers[i]**5
     numbers[i] += numbers[i]**2
-print(numbers[1])
+print(numbers[41])
